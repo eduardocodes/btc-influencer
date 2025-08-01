e316ea9071ed76c6fb140ca9a54a6912b6207ee2
Eduardo <eduardovarjaor@gmail.com>
2025-08-01 19:41:03 -0300

    chore(supabase): remove unused console logs

--- src/lib/supabase/admin.ts
+++ src/lib/supabase/admin.ts
@@ -1,8 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-console.log('NEXT_PUBLIC_SUPABASE_URL', process.env.NEXT_PUBLIC_SUPABASE_URL);
-console.log('SUPABASE_SERVICE_ROLE_KEY', process.env.SUPABASE_SERVICE_ROLE_KEY);
-
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
   process.env.SUPABASE_SERVICE_ROLE_KEY!,
